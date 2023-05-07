2aa479a9645b4033185e11e3a12ec8a57bf42a3d
Ghaith AlMasri <ghaithalmasri@Ghaiths-MacBook-Air.local>
2023-05-07 20:01:33 +0200

    added shirt and logo

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -1,14 +1,42 @@
-import {useFrame} from '@react-three/fiber'
-import {easing} from 'maath'
-import { useSnapshot } from 'valtio'
+/* eslint-disable react/prop-types */
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
 
+import state from "../store";
 
-import state from '../store'
+const CameraRig = ({ children }) => {
+  const group = useRef();
+  const snap = useSnapshot(state);
 
-const CameraRig = () => {
-  return (
-    <div>CameraRig</div>
-  )
-}
+  useFrame((state, delta) => {
+    const isBreakpoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+    let targetPosition = [-0.4, 0, 2];
 
-export default CameraRig
+    if (snap.intro) {
+      isBreakpoint
+        ? (targetPosition = [0, 0, 2])
+        : (targetPosition = [-0.4, 0, 2]);
+      isMobile
+        ? (targetPosition = [0, 0.2, 2.5])
+        : (targetPosition = [-0.4, 0, 2]);
+    } else {
+      isMobile ? (targetPosition = [0, 0, 2.5]) : (targetPosition = [0, 0, 2]);
+    }
+
+    easing.damp3(state.camera.position, targetPosition, 0.25, delta);
+
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25,
+      delta
+    );
+  });
+
+  return <group ref={group}>{children}</group>;
+};
+
+export default CameraRig;
--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,15 +1,14 @@
-import {easing} from 'maath'
-import {useSnapshot} from 'valtio'
-import {useFrame} from '@react-three/fiber'
-import {Decal, useGLTF, useTexture} from '@react-three/drei'
-import state from '../store'
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
+import { useFrame } from "@react-three/fiber";
+import { Decal, useGLTF, useTexture } from "@react-three/drei";
+import state from "../store";
 const Shirt = () => {
-  const snap = useSnapshot(state)
-  const {nodes, materials} = useGLTF("/shirt_baked.glb")
+  const snap = useSnapshot(state);
+  const { nodes, materials } = useGLTF("/shirt_baked.glb");
 
-  const logoTexture = useTexture(snap.logoDecal)
-  const fullTexture = useTexture(snap.fullDecal)
-
+  const logoTexture = useTexture(snap.logoDecal);
+  const fullTexture = useTexture(snap.fullDecal);
 
   return (
     <group>
@@ -19,11 +18,27 @@ const Shirt = () => {
         material={materials.lambert1}
         material-roughness={1}
         dispose={null}
-        >
-        
+      >
+        {snap.isFullTexture && (
+          <Decal
+            position={[0, 0, 0]}
+            rotation={[0, 0, 0]}
+            scale={1}
+            map={fullTexture}
+          />
+        )}
+
+        {snap.isLogoTexture && (
+          <Decal
+            position={[0.1, 0.05, 0.15]}
+            rotation={[0, 0.5, 0]}
+            scale={0.10}
+            map={logoTexture}
+          />
+        )}
       </mesh>
     </group>
-  )
-}
+  );
+};
 
-export default Shirt
+export default Shirt;
